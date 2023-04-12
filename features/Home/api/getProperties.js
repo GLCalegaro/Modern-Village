b89f33176715562b71a73f47ac95eac35dc934ef
GLCalegaro <giovaluiza@gmail.com>
2023-04-12 13:10:44 -0300

    Ajuste axios

--- features/Home/api/getProperties.js
+++ features/Home/api/getProperties.js
@@ -1,4 +1,4 @@
-import {axios} from "@/lib/axios";
+import axios from "@/lib/axios";
 
 export const getProperties = async (num) => {
     const { data } = await axios.get("/properties/list", {
--- features/Property/api/getProperty.js
+++ features/Property/api/getProperty.js
@@ -1,4 +1,4 @@
-import {axios} from "@/lib/axios";
+import axios from "@/lib/axios";
 
 export const getProperty = async (id) => {
     const {data} = await axios.get("/properties/detail", {
